4d2d2c806f37bf358fe7e8ddc20d83004e9ee4c1
John Vazquez <vazquezjohn279s@gmail.com>
2025-11-24 03:50:27 -0600

    added map, js, css, and fixes some page 3 elements

---
+++ script.js
@@ -0,0 +1,48 @@
+function init() {
+
+  var markers = [
+    {
+      locationName: 'Estadio Azteca',
+      lat1: 19.3029,
+      lng1: -99.1505,
+      address1: '10 W Calz. de Tlalpan 3465, Sta. Úrsula Coapa, Coyoacán, 04650 Ciudad de México, CDMX, Mexico St,',
+      
+      locationName: 'Estadio Akron',
+      lat2: 20.681667,
+      lng2:  -103.462778,
+      address2: 'Cto. J.V.C. 2800, El Bajío, 45014 Zapopan, Jal., MexicoCircuito JVC 2800, Zapopan, Jalisco, 45019, Mexico'
+      
+    }
+  ];
+
+  var MapCord = { lat: 19.3029, lng: -99.1505 }
+
+  var mapOptions = {
+    center: MapCord,
+    zoom: 17,
+    disableDefaultUI: true,
+    zoomControl: true,
+  }
+
+  var map = new google.maps.Map(document.getElementById('Map'), mapOptions);
+
+  var Azteca = new google.maps.Marker({
+    position: { lat: markers[0]['lat1'], lng: markers[0]['lng1'] },
+    map: map,
+  });
+  var Akron = new google.maps.Marker({
+    position: { lat: markers[0]['lat2'], lng: markers[0]['lng2'] },
+    map: map,
+  });
+
+  var alertbox = document.getElementById("button").addEventListener("click", popup);
+
+  function popup() {
+    alert("If you have questions, contact me at: jVazquez19@hawk.illinoistech.edu");
+    document.getElementById
+  }
+
+}
+
+
+window.addEventListener('load', init);
